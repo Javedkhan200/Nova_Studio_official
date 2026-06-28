23b90f8c7f9b9b48a3beecc5f21da233c4edc7ab
Javedkhan200 <mj1234aved1234@gmail.com>
2026-06-28 18:14:24 +0530

    Full System Update: Engine, Copyrights, Colors & Installer Fixes

--- core/nova_compiler.cpp
+++ core/nova_compiler.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
 #include <regex>
-#include <vector>
+#include <sstream>
 using namespace std;
-
 #define CYAN "\033[1;36m"
 #define RESET "\033[0m"
 #define YELLOW "\033[1;33m"
@@ -17,23 +16,18 @@ int main() {
     while (true) {
         cout << YELLOW << ">>> " << RESET;
         if (!getline(cin, code) || code == "exit") break;
-        
-        // Meta Commands
         if (code == "copyright") show_copyright();
         else if (code == "credits") cout << "Architect: Javed. Nova Engine v110.\n";
         else if (code == "license") cout << "Released under Nova Universal License.\n";
-        
-        // Nova Syntax: Nova.show("text")
         else if (regex_match(code, regex(R"(Nova\.show\(\"([^\"]+)\"\))"))) {
             smatch m; regex_search(code, m, regex(R"(\"([^\"]+)\")"));
             cout << m[1] << endl;
         }
-        // Math Logic: 345+457
         else if (regex_match(code, regex(R"(\d+\+\d+)"))) {
             int a, b; char op; stringstream ss(code); ss >> a >> op >> b;
             cout << a + b << endl;
         }
-        else { cout << "Nova Engine: Input received -> " << code << endl; }
+        else { cout << "Nova Syntax Engine: " << code << endl; }
     }
     return 0;
 }
