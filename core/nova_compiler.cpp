615f05ff9c6a9f5ea1de94c99b1001a7661e38f9
Javedkhan200 <mj1234aved1234@gmail.com>
2026-06-28 18:09:06 +0530

    Final Update: Copyrights, Colors & Docs Structure

--- core/nova_compiler.cpp
+++ core/nova_compiler.cpp
@@ -1,33 +1,21 @@
 #include <iostream>
 #include <string>
 #include <regex>
-#include <map>
 using namespace std;
-map<string, string> nova_memory;
 #define CYAN "\033[1;36m"
 #define RESET "\033[0m"
 #define YELLOW "\033[1;33m"
-
-void execute_line(string line) {
-    smatch m;
-    if (line == "copyright") { cout << "Copyright (c) 2026 Nova Studio. All Rights Reserved.\n"; return; }
-    if (line == "credits") { cout << "Architect: Javed. Engine v110.\n"; return; }
-    if (line == "license") { cout << "Released under Nova Universal License.\n"; return; }
-    
-    if (regex_search(line, m, regex(R"(Nova\.ask\.user\(\"([a-zA-Z0-9_]+):\"(.*)\))"))) {
-        cout << m[2] << " "; string val; getline(cin, val); nova_memory[m[1]] = val;
-    }
-    else if (regex_search(line, m, regex(R"(Nova\.show\(\"([^\"]+)\"\))"))) { cout << m[1] << endl; }
-    else if (regex_search(line, m, regex(R"(Nova\.show\(([a-zA-Z0-9_]+)\))"))) { cout << nova_memory[m[1]] << endl; }
-    else if (!line.empty()) { cout << "SyntaxError: Nova nahi samajh paya!" << endl; }
-}
 int main() {
-    cout << CYAN << "Nova 110.0 [Advanced Syntax] Ready." << RESET << endl;
+    cout << CYAN << "Nova 110.0 | Copyright (c) 2026 Nova Studio. All Rights Reserved." << RESET << endl;
+    cout << "Type \"help\", \"copyright\", \"credits\" or \"license\"." << endl;
     string code;
     while (true) {
         cout << YELLOW << ">>> " << RESET;
         if (!getline(cin, code) || code == "exit") break;
-        execute_line(code);
+        if (code == "copyright") cout << "Copyright (c) 2026 Nova Studio. All Rights Reserved.\n";
+        else if (code == "credits") cout << "Architect: Javed. Nova Engine v110.\n";
+        else if (code == "license") cout << "Released under Nova Universal License.\n";
+        else cout << "Nova Syntax Engine Ready. Input: " << code << endl;
     }
     return 0;
 }
