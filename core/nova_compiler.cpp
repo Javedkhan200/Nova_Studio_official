e028467b3504fd04ae5bd0db4b7106a4541ec9b0
Javedkhan200 <mj1234aved1234@gmail.com>
2026-06-28 18:08:33 +0530

    Nova Final Build: Copyright, Color Syntax & Docs Fix

--- core/nova_compiler.cpp
+++ core/nova_compiler.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <map>
 using namespace std;
+map<string, string> nova_memory;
+#define CYAN "\033[1;36m"
+#define RESET "\033[0m"
+#define YELLOW "\033[1;33m"
+
 void execute_line(string line) {
+    smatch m;
     if (line == "copyright") { cout << "Copyright (c) 2026 Nova Studio. All Rights Reserved.\n"; return; }
     if (line == "credits") { cout << "Architect: Javed. Engine v110.\n"; return; }
-    if (line == "license") { cout << "Nova Universal License.\n"; return; }
-    if (line.find("Nova.show") != string::npos) { cout << line.substr(line.find("\"")+1, line.find_last_of("\"") - line.find("\"") - 1) << endl; }
+    if (line == "license") { cout << "Released under Nova Universal License.\n"; return; }
+    
+    if (regex_search(line, m, regex(R"(Nova\.ask\.user\(\"([a-zA-Z0-9_]+):\"(.*)\))"))) {
+        cout << m[2] << " "; string val; getline(cin, val); nova_memory[m[1]] = val;
+    }
+    else if (regex_search(line, m, regex(R"(Nova\.show\(\"([^\"]+)\"\))"))) { cout << m[1] << endl; }
+    else if (regex_search(line, m, regex(R"(Nova\.show\(([a-zA-Z0-9_]+)\))"))) { cout << nova_memory[m[1]] << endl; }
+    else if (!line.empty()) { cout << "SyntaxError: Nova nahi samajh paya!" << endl; }
 }
 int main() {
-    cout << "Nova 110.0 [Stable]. Type 'copyright', 'credits', or 'license'.\n>>> ";
+    cout << CYAN << "Nova 110.0 [Advanced Syntax] Ready." << RESET << endl;
     string code;
-    while(getline(cin, code)) { execute_line(code); cout << ">>> "; }
+    while (true) {
+        cout << YELLOW << ">>> " << RESET;
+        if (!getline(cin, code) || code == "exit") break;
+        execute_line(code);
+    }
     return 0;
 }
