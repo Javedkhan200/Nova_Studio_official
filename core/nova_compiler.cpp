c18c041293df4bd290b5253d2a297645b98ccecf
Javedkhan200 <mj1234aved1234@gmail.com>
2026-06-28 18:11:57 +0530

    All-in-One: Engine Fixed, Math Logic Added, Docs Fixed

--- core/nova_compiler.cpp
+++ core/nova_compiler.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <vector>
 using namespace std;
+
 #define CYAN "\033[1;36m"
 #define RESET "\033[0m"
 #define YELLOW "\033[1;33m"
+
+void show_copyright() { cout << CYAN << "Nova 110.0 | Copyright (c) 2026 Nova Studio. All Rights Reserved." << RESET << endl; }
+
 int main() {
-    cout << CYAN << "Nova 110.0 | Copyright (c) 2026 Nova Studio. All Rights Reserved." << RESET << endl;
+    show_copyright();
     cout << "Type \"help\", \"copyright\", \"credits\" or \"license\"." << endl;
     string code;
     while (true) {
         cout << YELLOW << ">>> " << RESET;
         if (!getline(cin, code) || code == "exit") break;
-        if (code == "copyright") cout << "Copyright (c) 2026 Nova Studio. All Rights Reserved.\n";
+        
+        // Meta Commands
+        if (code == "copyright") show_copyright();
         else if (code == "credits") cout << "Architect: Javed. Nova Engine v110.\n";
         else if (code == "license") cout << "Released under Nova Universal License.\n";
-        else cout << "Nova Syntax Engine Ready. Input: " << code << endl;
+        
+        // Nova Syntax: Nova.show("text")
+        else if (regex_match(code, regex(R"(Nova\.show\(\"([^\"]+)\"\))"))) {
+            smatch m; regex_search(code, m, regex(R"(\"([^\"]+)\")"));
+            cout << m[1] << endl;
+        }
+        // Math Logic: 345+457
+        else if (regex_match(code, regex(R"(\d+\+\d+)"))) {
+            int a, b; char op; stringstream ss(code); ss >> a >> op >> b;
+            cout << a + b << endl;
+        }
+        else { cout << "Nova Engine: Input received -> " << code << endl; }
     }
     return 0;
 }
